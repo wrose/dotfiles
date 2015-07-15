1fa3e64506d14ad23836419e4ff4ca58e1571ed2
William Rose <william.rose@capitalone.com>
2015-07-15 11:44:43 -0400

    add c1-git and tweak some things

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,4 @@
 #!/usr/local/bin/fish
 set -gx PATH /usr/local/bin $PATH $HOME/.rvm/bin
 
-/usr/local/bin/bash source $HOME/.yodle-dev-shell-utils/conf/sh/all
-
 test -s /Users/wrose/.nvm-fish/nvm.fish; and source /Users/wrose/.nvm-fish/nvm.fish
