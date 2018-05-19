7bfd6f05a04e272acc7ac61d628ac08b919d4ed4
William Rose <will@williamro.se>
2018-05-19 19:49:23 -0400

    ssh agent should be working now

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,7 @@
 if not functions -q fundle; eval (curl -sfL https://git.io/fundle-install); end
 
 # Path to your oh-my-fish.
-set fish_path $HOME/.oh-my-fish
+#set fish_path $HOME/.oh-my-fish
 
 set SHELL /usr/local/bin/fish
 
@@ -9,17 +9,17 @@ set SHELL /usr/local/bin/fish
 #set fish_custom $HOME/dotfiles/oh-my-fish
 
 # Load oh-my-fish configuration.
-. $fish_path/oh-my-fish.fish
+#. $fish_path/oh-my-fish.fish
 
 # Custom plugins and themes may be added to ~/.oh-my-fish/custom
 # Plugins and themes can be found at https://github.com/oh-my-fish/
-Theme 'bobthefish'
-Plugin 'theme'
+#Theme 'bobthefish'
+#Plugin 'theme'
 
-if status --is-interactive
-  set -l IFS # this temporarily clears IFS, which disables the newline-splitting
-  eval (keychain --eval --quiet -Q id_rsa)
+if begin; test -z (command pgrep ssh-agent); and not test -S $SSH_AUTH_SOCK; end
+	eval (command ssh-agent -c | sed -E 's/^setenv (.+);$/set \1; set -Ux \1;/')
 end
 
-#source $HOME/.keychain/subayai-fish
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+thefuck --alias | source 
+set -x THEFUCK_OVERRIDDEN_ALIASES 'git'
+set -g fish_user_paths "/usr/local/opt/ruby@2.3/bin" $fish_user_paths
