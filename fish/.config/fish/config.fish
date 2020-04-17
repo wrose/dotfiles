be082884a9043d497aab8af3acd55bb98073f88e
William Rose <will@williamro.se>
2020-04-17 07:21:28 -0400

    mostly updating Brewfile

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,9 @@
 
 status --is-interactive; and . (jump shell | psub)
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
