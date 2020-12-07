c612c72cf57f2492d80240c4f8c6cef0472814e6
William Rose <will@williamro.se>
2020-12-07 11:44:01 -0800

    Updated Brewfile

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -7,3 +7,4 @@ if not functions -q fisher
     fish -c fisher
 end
 set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+set -g fish_user_paths "/usr/local/opt/mysql-client/bin" $fish_user_paths
